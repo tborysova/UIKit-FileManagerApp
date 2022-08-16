8d01d5b15cee435a8b9e5c7c992ed7c9befb6e98
tborysova <borysova.tetiana@gmail.com>
2022-08-16 15:25:24 +0200

    Added loading images with UIImage

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -8,13 +8,19 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
 
     @IBOutlet var imageView: UIImageView!
     
+    var selectedImage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
 
-        // Do any additional setup after loading the view.
     }
     
 
